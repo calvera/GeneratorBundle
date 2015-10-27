04ec419c3fa87e68be8323c5ae0762038e7e4f1d
Karel Sommer <karel.sommer@gmail.com>
2015-10-27 14:45:32 +0100

    temporary fix for csrf_filter/csrf_token

--- Twig/Extension/CsrfTokenExtension.php
+++ Twig/Extension/CsrfTokenExtension.php
@@ -29,7 +29,8 @@ class CsrfTokenExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'csrf_token' => new \Twig_SimpleFilter('csrf_filter', array($this, 'getCsrfToken')),
+            'csrf_token' => new \Twig_SimpleFilter('csrf_token', array($this, 'getCsrfToken')),
+            'csrf_filter' => new \Twig_SimpleFilter('csrf_filter', array($this, 'getCsrfToken')),
         );
     }
 
